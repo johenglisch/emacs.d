2ddab0923f0a9680c0a554c6c5239fc20da481de
Johannes Englisch <cyberjoe0815@hotmail.com>
2015-12-09 22:16:24 +0100

    Org-mode: added folder for agenda files

--- elisp/orgmode-config.el
+++ elisp/orgmode-config.el
@@ -2,5 +2,14 @@
 
 (require 'org)
 
+(defvar init-agenda-dir nil)
+(setq init-agenda-dir
+      (if (eq system-type 'windows-nt)
+          "~/Documents/org-mode/"
+        "~/.org-mode/"))
+
+(when (file-directory-p init-agenda-dir)
+  (setq org-agenda-files (list init-agenda-dir)))
+
 (setq org-list-allow-alphabetical t)
 (add-hook 'org-mode-hook #'font-lock-mode)
