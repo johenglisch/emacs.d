931836b500b77c33d4d656fc1122f562b9756dc4
Johannes Englisch <cyberjoe0815@hotmail.com>
2016-03-15 11:03:34 +0100

    Added more refined worflow states

--- elisp/orgmode-config.el
+++ elisp/orgmode-config.el
@@ -8,6 +8,9 @@
           "~/Documents/org-mode/"
         "~/.org-mode/"))
 
+(setq org-todo-keywords
+      '((sequence "TODO" "MIGHTDO" "WAITING" "|" "DONE")))
+
 (when (file-directory-p init-agenda-dir)
   (setq org-agenda-files (list init-agenda-dir)))
 
