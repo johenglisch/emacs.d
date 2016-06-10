da265e363a989437fa76d4e6415de56154e5e000
Johannes Englisch <johannes.englisch@uni-leipzig.de>
2016-06-10 09:39:36 +0200

    powerline: set airline theme

--- elisp/style-config.el
+++ elisp/style-config.el
@@ -44,6 +44,19 @@
   (eval-after-load "autorevert" '(diminish 'auto-revert-mode)))
 
 (when (require 'powerline nil t)
+
   (if (require 'powerline-evil nil t)
       (powerline-evil-vim-color-theme)
-    (powerline-default-theme)))
+    (powerline-default-theme))
+
+  (when (require 'airline-themes nil t)
+
+    (setq airline-utf-glyph-separator-left     #xe0b0
+          airline-utf-glyph-separator-right    #xe0b2
+          airline-utf-glyph-subseparator-left  #xe0b1
+          airline-utf-glyph-subseparator-right #xe0b3
+          airline-utf-glyph-branch             #xe0a0
+          airline-utf-glyph-readonly           #xe0a2
+          airline-utf-glyph-linenumber         #xe0a1)
+
+    (load-theme 'airline-powerlineish t t)))
