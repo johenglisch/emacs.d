71e24e4244464ab3a6a257eed582755cf9a90c6c
Johannes Englisch <cyberjoe0815@hotmail.com>
2016-10-13 21:10:44 +0200

    added org-mode/contrib to load-path

--- elisp/orgmode-config.el
+++ elisp/orgmode-config.el
@@ -2,9 +2,11 @@
 
 ;; The version of org-mode shipped with emacs doesn't know how to cider..
 ;; Git repo: git://orgmode.org/org-mode.git
-(let ((org-mode-dir (expand-file-name "org-mode/lisp" user-emacs-directory)))
-  (if (file-directory-p org-mode-dir)
-      (add-to-list 'load-path org-mode-dir)))
+(let ((org-mode-dir (expand-file-name "org-mode/lisp" user-emacs-directory))
+      (contrib-dir  (expand-file-name "org-mode/contrib/lisp" user-emacs-directory)))
+  (when (file-directory-p org-mode-dir)
+    (add-to-list 'load-path org-mode-dir)
+    (add-to-list 'load-path contrib-dir)))
 
 (require 'org)
 (require 'ob-clojure)
