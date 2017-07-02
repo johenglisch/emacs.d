f39d3cb6e0234705c86c5b438aef780ffdb3b5a0
Johannes Englisch <cyberjoe0815@hotmail.com>
2017-07-02 11:57:46 +0200

    use https for packages

--- init.el
+++ init.el
@@ -50,9 +50,9 @@
 ;;; Package Management -----------------------------------------------
 
 (require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")))
+                         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
 (setq package-selected-packages
