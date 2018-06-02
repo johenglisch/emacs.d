7a7ead4d7df395cf77c0f2258946ab26f3e818dd
Johannes Englisch <cyberjoe0815@hotmail.com>
2018-06-02 21:08:20 +0200

    dont use this anymore

--- elisp/style-config.el
+++ elisp/style-config.el
@@ -6,7 +6,6 @@
 
 (global-font-lock-mode 0)
 (add-hook 'term-mode-hook #'font-lock-mode)
-(add-hook 'powershell-mode-hook #'font-lock-mode)
 (add-hook 'tex-mode-hook #'font-lock-mode)
 (add-hook 'rst-mode-hook #'font-lock-mode)
 (add-hook 'ag-mode-hook #'font-lock-mode)
