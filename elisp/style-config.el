7d6a92d34155f16453142e9516ed3ff8d93ae616
Johannes Englisch <cyberjoe0815@hotmail.com>
2018-10-26 10:17:52 +0200

    diminish: company was removed

--- elisp/style-config.el
+++ elisp/style-config.el
@@ -44,7 +44,6 @@
 
 (when (require 'diminish nil t)
   (eval-after-load "autorevert" '(diminish 'auto-revert-mode))
-  (eval-after-load "company"    '(diminish 'company-mode))
   (eval-after-load "eldoc"      '(diminish 'eldoc-mode))
   (eval-after-load "paredit"    '(diminish 'paredit-mode))
   (eval-after-load "undo-tree"  '(diminish 'undo-tree-mode))
