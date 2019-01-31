165afe552192433dcdc3e5a80b7be0b213ea2ba8
Johannes Englisch <cyberjoe0815@hotmail.com>
2019-01-31 20:52:40 +0100

    integrated helpers into init.el

--- init.el
+++ init.el
@@ -38,8 +38,6 @@
 (setq projectile-known-projects-file (expand-file-name "projectile-bookmarks.eld" init-tmp-dir))
 (setq projectile-cache-file          (expand-file-name "projectile.cache" init-tmp-dir))
 
-(add-to-list 'load-path (expand-file-name "elisp/" user-emacs-directory))
-
 (if (eq system-type 'windows-nt)
     (setq default-directory "~/"))
 
@@ -57,6 +55,13 @@
         flycheck paredit yasnippet
         auctex auctex-latexmk cider fountain-mode json-mode
         markdown-mode))
+
+
+;;; Helper Functions -------------------------------------------------
+
+(defun init-byte-compile-current ()
+  (interactive)
+  (byte-compile-file (buffer-file-name)))
 
 
 ;;; Appearance -------------------------------------------------------
@@ -286,10 +291,33 @@
                           ("sentend" "\\@" nil "" "" "" "")))
 
 
-;;; Elisp files ------------------------------------------------------
+;;; Key Bindings -----------------------------------------------------
 
-(load-library "random-functions")
+;; General
 
-(load-library "key-bindings")
+(global-set-key (kbd "C-c e")
+                (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
 
-(load "local" t)
+(global-set-key (kbd "C-c f") #'find-file-at-point)
+
+(global-set-key (kbd "C-c t") #'term)
+
+;; Plugins
+
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c c") #'org-capture)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c b") #'org-iswitchb)
+
+;; for some reason, <menu> is called <apps> on windows...
+(unless (package-installed-p 'smex)
+  (global-set-key (kbd "<apps>") #'execute-extended-command))
+
+(when (package-installed-p 'smex)
+  (global-set-key (kbd "M-x") #'smex)
+  (global-set-key (kbd "<menu>") #'smex)
+  (global-set-key (kbd "<apps>") #'smex)
+  (global-set-key (kbd "<execute>") #'smex))
+
+(when (package-installed-p 'magit)
+  (global-set-key (kbd "C-c g") #'magit-status))
