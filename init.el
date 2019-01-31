20c8e8cab3c10c2aa4cf9252ce13e06c7dce5eb1
Johannes Englisch <cyberjoe0815@hotmail.com>
2019-01-31 20:27:25 +0100

    integrated style config into init.el

--- init.el
+++ init.el
@@ -59,11 +59,50 @@
         markdown-mode))
 
 
+;;; Appearance -------------------------------------------------------
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(setq eol-mnemonic-dos "\\")
+(setq eol-mnemonic-unix ":")
+(setq eol-mnemonic-mac "/")
+(setq eol-mnemonic-undecided ":")
+
+(setq inhibit-startup-screen t)
+
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(fringe-mode 0)
+
+(column-number-mode t)
+
+(when window-system
+  (load-theme 'deeper-blue))
+
+(setq frame-title-format "%b – Emacs")
+(setq frame-resize-pixelwise t)
+
+(cl-case system-type
+  ('gnu/linux  (set-frame-font "Hack-10" nil t))
+  ('windows-nt (set-frame-font "Consolas-10" nil t)))
+
+(when (eq system-type 'windows-nt)
+    (setq inhibit-compacting-font-caches t))
+
+(global-font-lock-mode 0)
+(add-hook 'term-mode-hook #'font-lock-mode)
+(add-hook 'tex-mode-hook #'font-lock-mode)
+(add-hook 'rst-mode-hook #'font-lock-mode)
+(add-hook 'ag-mode-hook #'font-lock-mode)
+(add-hook 'cider-repl-mode-hook #'font-lock-mode)
+(add-hook 'fountain-mode-hook #'font-lock-mode)
+
+
 ;;; Elisp files ------------------------------------------------------
 
 (load-library "random-functions")
 
-(load-library "style-config")
 (load-library "editing-config")
 (load-library "fuzzymatch-config")
 (load-library "orgmode-config")
