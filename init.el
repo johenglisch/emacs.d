c1f36c6de0b889ad653eb4044175af2f2a2d22c9
Johannes Englisch <johannes.englisch@t-online.de>
2023-04-27 17:43:34 +0200

    paredit keeps breaking repls...

--- init.el
+++ init.el
@@ -238,12 +238,14 @@
 
   (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
   (add-hook 'eval-expression-minibuffer #'enable-paredit-mode)
-  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+  ;;(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
   (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
   (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
   (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
   (add-hook 'clojure-mode-hook          #'enable-paredit-mode)
-  (add-hook 'cider-repl-mode-hook       #'enable-paredit-mode))
+  ;;(add-hook 'cider-repl-mode-hook       #'enable-paredit-mode)
+  ;;(add-hook 'geiser-repl-mode-hook      #'enable-paredit-mode)
+  )
 
 ;; Projectile
 
